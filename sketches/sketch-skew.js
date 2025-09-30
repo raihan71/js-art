b034d7cefbcfddf8a7d5bda1b40f0092117608bf
Raihan Nismara <raihan@d3labs.io>
2025-09-30 07:42:12 +0700

    chore: add clipmask with unsimetris rect

--- sketches/sketch-skew.js
+++ sketches/sketch-skew.js
@@ -1,26 +1,24 @@
 import canvasSketch from 'canvas-sketch';
 import math from 'canvas-sketch-util/math';
 import random from 'canvas-sketch-util/random';
+import Color from 'canvas-sketch-util/color';
 import risoColors from 'riso-colors';
 
 const settings = {
   dimensions: [1080, 1080],
-  animate: true,
-  duration: 4,
-  fps: 60,
+  // animate: true,
+  // duration: 4,
+  // fps: 60,
 };
 
 const sketch = ({ context, width, height }) => {
-  let x, y, w, h, fill, stroke;
+  let x, y, w, h, fill, stroke, blend;
 
-  const num = 25;
+  const num = 40;
   const degrees = -35;
   const rects = [];
 
   const rectColors = [
-    random.pick(risoColors),
-    random.pick(risoColors),
-    random.pick(risoColors),
     random.pick(risoColors),
     random.pick(risoColors),
     random.pick(risoColors),
@@ -41,48 +39,54 @@ const sketch = ({ context, width, height }) => {
 
     fill = random.pick(rectColors).hex;
     stroke = random.pick(rectColors).hex;
-    rects.push({ x, y, w, h, fill, stroke });
+    blend = random.value() > 0.5 ? 'overlay' : 'source-over';
+    rects.push({ x, y, w, h, fill, stroke, blend });
   }
 
   return ({ context, width, height }) => {
     context.fillStyle = bgColor;
     context.fillRect(0, 0, width, height);
+    context.save();
+    context.translate(width * 0.5, height * 0.5);
+    context.beginPath();
+    context.moveTo(0, -300);
+    context.lineTo(300, 200);
+    context.lineTo(-300, 200);
+    context.closePath();
+    context.lineWidth = 10;
+    context.strokeStyle = 'black';
+    context.stroke();
+    context.clip();
 
     rects.forEach((rect) => {
-      const { x, y, w, h, stroke, fill } = rect;
+      const { x, y, w, h, stroke, fill, blend } = rect;
+      let shadowColor;
       context.save();
+      context.translate(-width * 0.5, -height * 0.5);
       context.translate(x, y);
       context.strokeStyle = stroke;
       context.lineWidth = 10;
       context.fillStyle = fill;
+      context.globalCompositeOperation = blend;
       drawSkewed({ context, w, h, degrees });
-      context.shadowColor = 'rgba(0,0,0,0.5)';
+      shadowColor = Color.offsetHSL(fill, 0, 0, -20);
+      shadowColor.rgba[3] = 0.5;
+      context.shadowColor = Color.style(shadowColor.rgba);
       context.shadowOffsetX = 10;
-      context.shadowOffsetY = 20;
+      context.shadowOffsetY = 10;
       context.shadowBlur = 10;
       context.fill();
       context.shadowColor = null;
       context.stroke();
+      context.globalCompositeOperation = 'source-over';
+      context.lineWidth = 2;
+      context.strokeStyle = random.pick(rectColors).hex;
+      context.stroke();
       context.restore();
     });
+
+    context.restore();
   };
-
-  // // context.beginPath();
-  // // context.moveTo(0, 0);
-  // // context.lineTo(w, 0);
-  // // context.lineTo(w, h);
-  // // context.lineTo(0, h);
-  // // context.closePath();
-  // // context.stroke();
-  // context.beginPath();
-  // context.moveTo(0, 0);
-  // context.lineTo(rx, ry);
-  // context.lineTo(rx, ry + h);
-  // context.lineTo(0, h);
-  // context.closePath();
-  // context.stroke();
-
-  // context.restore();
 };
 
 const drawSkewed = ({ context, w = 600, h = 200, degrees = -45 }) => {
@@ -100,6 +104,19 @@ const drawSkewed = ({ context, w = 600, h = 200, degrees = -45 }) => {
   context.closePath();
 
   context.restore();
+};
+
+const drawPolygon = (context, radius = 100, sides = 3) => {
+  const slice = (Math.PI * 2) / sides;
+  context.beginPath();
+  context.moveTo(0, -radius);
+  for (let i = 1; i < sides; i++) {
+    const theta = i * slice - Math.PI / 2;
+    const x = Math.cos(theta) * radius;
+    const y = Math.sin(theta) * radius;
+    context.lineTo(x, y);
+  }
+  context.closePath();
 };
 
 canvasSketch(sketch, settings);
